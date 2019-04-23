2262b5fa8f4ff0066021d3e1e57fa7bc724969bc
bmgaber <bmgaberah@gmail.com>
2019-04-23 16:50:06 +0200

    started pass 2:

--- SIC-XE Assembler & Simulator/src/Assembling/Main.java
+++ SIC-XE Assembler & Simulator/src/Assembling/Main.java
@@ -6,10 +6,12 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Set;
 
 public class Main {
-        public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         String fileName = "a_example.txt";
         String fileName2 = "b_example.txt";
         File file = new File(fileName);
@@ -19,6 +21,19 @@ public class Main {
         HashMap symtbl = new HashMap<String, Integer>();
         HashMap littbl = new HashMap<Integer, Integer>();
         Pass1.flow(br, symtbl, writer, littbl);
+        writer.write("\nEnd of first pass\n");
+        Set Table = symtbl.entrySet();
+
+        for(Object i : Table)
+        {
+            String[] division = i.toString().split("=");
+            for(String w : division)
+            {
+                writer.write(w);
+            }
+            writer.write("\n");
+        }
+        Pass2.flow(br, writer);
         br.close();
         writer.close();
     }
--- SIC-XE Assembler & Simulator/src/Assembling/Pass2.java
+++ SIC-XE Assembler & Simulator/src/Assembling/Pass2.java
@@ -1,5 +1,57 @@
 package Assembling;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+
 public class Pass2 {
+    public Pass2() {
+    }
+
+    public static void flow(BufferedReader br, BufferedWriter wr) throws IOException {
+        String line = br.readLine();
+        while ((line = br.readLine()) != null) {
+            while (line.charAt(0) == '.') {
+                line = br.readLine();
+            }
+            int num = doLine(line);
+            if (num == 1) {
+                if (line.length() > 14) {
+                    wr.write("E" + Pass1.hexafy(Integer.parseInt(line.substring(17, line.length()))));
+                } else {
+                    wr.write("E");
+                }
+                break;
+            }
+        }
+        wr.write("\nSuccsessful Assembly");
+        return;
+    }
+
+    public static int doLine(String line) {
+        String first = line.substring(0, 7);
+        String operation;
+        String operand;
+        if (line.length() > 14) {
+            operation = line.substring(9, 14).trim().toLowerCase();
+            operand = line.substring(17, line.length());
+        } else {
+            operation = line.substring(9, line.length()).toLowerCase();
+            operand = null;
+        }
+        if (operand.trim().toLowerCase().equals("end"))
+            return 1;
+        switch (operation) {
+            case "word" :
+            {
+
+            }
+            case "byte" :
+            {
+
+            }
+        }
+        return 0;
+    }
 
 }
